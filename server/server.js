149bf86b875a3610721b622c85de606cfdbb0213
sebavicente93 <svicente_93@hotmail.com>
2017-01-14 21:46:43 -0300

    Test DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -52,7 +52,7 @@ app.get('/todos/:id',(req,res) => {
 
 });
 
-app.delete('/delete/:id',(req,res) => {
+app.delete('/todos/:id',(req,res) => {
   var id= req.params.id;
 
   if(!ObjectId.isValid(id)){
@@ -63,7 +63,7 @@ app.delete('/delete/:id',(req,res) => {
     if (!todo){
       return res.status(404).send();
     }
-    res.status(200).send(todo);
+    res.status(200).send({todo});
   }).catch((e) => {
     res.status(404).send();
   });
